a4e04d4c1123582410af87ba9b6944af340c213c
oliver zulett <oliver.zulett@assuresoftbolivia.com>
2020-11-06 08:49:00 -0400

    feature(contorllers): health check controller

--- src/main/java/com/milankas/training/controller/HealthCheckController.java
+++ src/main/java/com/milankas/training/controller/HealthCheckController.java
@@ -1,4 +1,4 @@
-package com.milankascomposer.composerapi.controller;
+package com.milankas.training.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
